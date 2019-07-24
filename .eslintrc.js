b45dcb7fabbca24aabadb4d2d36d4d73355f70d8
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-24 11:39:31 -0400

    Installs eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,26 @@ module.exports = {
     node: true
   },
   extends: "eslint:recommended",
+  overrides: [
+    {
+      files: [
+        "**/*.spec.js"
+      ],
+      env: {
+        jest: true
+      },
+      plugins: [
+        "jest"
+      ],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ],
   parser: "babel-eslint",
   parserOptions: {
     sourceType: "module"
