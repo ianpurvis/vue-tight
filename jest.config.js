7b7c3e09310a185e76271b420fe21c79686db480
Ian Purvis <ian@purvisresearch.com>
2023-04-27 09:22:44 -0500

    Configure v8 coverage for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 export default {
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,cjs,mjs}'
+  ],
+  coverageProvider: 'v8',
   testEnvironment: 'node',
   moduleNameMapper: {
     '~(.*)$': '<rootDir>/$1',
