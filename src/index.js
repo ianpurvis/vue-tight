0437519a19bf058cdc0e3ea62f53208be431c41e
Ian Purvis <ian@purvisresearch.com>
2023-05-01 14:43:27 -0500

    Consolidate code to index module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import tighten from './tighten.js'
+const DefaultBlankMatcher = /^\s*$/
+
+function isBlank({ nodeValue }, matcher = DefaultBlankMatcher) {
+  return matcher.test(nodeValue)
+}
+
+function isText({ nodeType }) {
+  return nodeType === Node.TEXT_NODE
+}
+
+function tighten({ childElementCount, childNodes }) {
+  if (childElementCount > 0) {
+    [...childNodes]
+      .filter(n => isText(n) && isBlank(n))
+      .forEach(n => n.remove())
+  }
+}
 
 export default tighten
